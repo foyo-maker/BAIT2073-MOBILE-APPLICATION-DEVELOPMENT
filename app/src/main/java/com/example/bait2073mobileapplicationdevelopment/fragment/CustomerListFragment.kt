3c47a7db0baba83e56849db23794a6206095a6c3
foyo-maker <teefy-wm20@student.tarc.edu.my>
2023-08-04 02:47:12 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/com/example/bait2073mobileapplicationdevelopment/fragment/CustomerListFragment.kt
+++ app/src/main/java/com/example/bait2073mobileapplicationdevelopment/fragment/CustomerListFragment.kt
@@ -1,20 +1,21 @@
+package com.example.bait2073mobileapplicationdevelopment.fragment
+
+
 import android.app.Dialog
-import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
-import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
 import com.example.bait2073mobileapplicationdevelopment.R
-import com.example.bait2073mobileapplicationdevelopment.fragment.WorkoutFragment
 
-class HomeFragment : Fragment() {
+class CustomerListFragment : Fragment() {
 
     private lateinit var dialog: Dialog
 
@@ -23,17 +24,17 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_customer_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val showDialog: ImageView = view.findViewById(R.id.icon_add)
+        val showDialog: ImageView = view.findViewById(R.id.delete_img)
 
         // Create the Dialog here
         dialog = Dialog(requireContext())
-        dialog.setContentView(R.layout.custom_dialog_notification)
+        dialog.setContentView(R.layout.custom_dialog_warning)
         dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         dialog.setCancelable(false) // Optional
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.BLACK))
@@ -44,6 +45,7 @@ class HomeFragment : Fragment() {
         val cancel: Button = dialog.findViewById(R.id.btn_cancel)
 
         okay.setOnClickListener {
+            Log.i("cancel", "gg")
             Toast.makeText(requireContext(), "Okay", Toast.LENGTH_SHORT).show()
             dialog.dismiss()
         }
@@ -59,14 +61,5 @@ class HomeFragment : Fragment() {
         }
 
         okay.setBackgroundColor(requireContext().getColor(android.R.color.holo_red_light))
-
-        val personalizedRecommendCard: CardView = view.findViewById(R.id.personalizedRecommend)
-        personalizedRecommendCard.setOnClickListener {
-            // Replace the current fragment with the WorkoutFragment
-            val fragmentTransaction = parentFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.fragment_layout, WorkoutFragment())
-            fragmentTransaction.addToBackStack(null) // Add to back stack if you want to navigate back
-            fragmentTransaction.commit()
-        }
     }
 }
---
+++ app/src/main/java/com/example/bait2073mobileapplicationdevelopment/workout/WorkoutDetailsActivity.kt
@@ -0,0 +1,63 @@
+package com.example.bait2073mobileapplicationdevelopment.workout
+
+import android.content.Intent
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager2.widget.ViewPager2
+import com.example.bait2073mobileapplicationdevelopment.R
+import com.example.bait2073mobileapplicationdevelopment.ViewPagerAdapter
+import com.example.bait2073mobileapplicationdevelopment.databinding.AcitivityWorkoutDetailsBinding
+import com.example.bait2073mobileapplicationdevelopment.databinding.ActivityEventDetailsBinding
+import com.example.bait2073mobileapplicationdevelopment.databinding.ActivityRequestGenderBinding
+import com.example.bait2073mobileapplicationdevelopment.profile.RequestBmiActivity
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class WorkoutDetailsActivity : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var adapter: ViewPagerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivity_workout_details)
+
+        tabLayout = findViewById(R.id.tab_layout)
+        viewPager2 = findViewById(R.id.view_pager)
+
+        tabLayout.addTab(tabLayout.newTab().setText("Animation"))
+        tabLayout.addTab(tabLayout.newTab().setText("Video"))
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        adapter = ViewPagerAdapter(this)
+        viewPager2.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+            // Set the tab text based on the fragment position
+            when (position) {
+                0 -> tab.text = "Animation"
+                1 -> tab.text = "Video"
+            }
+        }.attach()
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+
+
+
+
+
+        val closeImg: ImageView = findViewById(R.id.close_img)
+        closeImg.setOnClickListener {
+            onBackPressed()
+        }
+    }
+}
