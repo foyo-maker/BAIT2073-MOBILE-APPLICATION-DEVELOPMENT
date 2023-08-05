70e5c5c7129dc198a1d85a4d6d6a884f53e2c88f
ChunYapBeep <127012228+ChunYapBeep@users.noreply.github.com>
2023-08-05 22:28:13 +0800

    cy part

---
+++ app/src/main/java/com/example/bait2073mobileapplicationdevelopment/fragment/DiseaseHospitalFragment.kt
@@ -0,0 +1,26 @@
+package com.example.bait2073mobileapplicationdevelopment.fragment;
+
+import android.os.Bundle
+
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+import com.example.bait2073mobileapplicationdevelopment.R
+
+class DiseaseHospitalFragment :Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_disease_hospital, container, false)
+    }
+
+
+}
