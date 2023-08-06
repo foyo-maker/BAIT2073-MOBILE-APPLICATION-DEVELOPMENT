8db9244813048287ae0b4e13e44c03464d95d9e3
HBS-LRN <hoobungseng0726@gmail.com>
2023-08-06 14:19:56 +0800

    HBS

---
+++ app/src/main/java/com/example/bait2073mobileapplicationdevelopment/fragment/StartPlan.kt
@@ -0,0 +1,76 @@
+package com.example.bait2073mobileapplicationdevelopment.fragment
+
+import android.graphics.Bitmap
+import android.graphics.BitmapShader
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Shader
+import android.graphics.drawable.BitmapDrawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.example.bait2073mobileapplicationdevelopment.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StartPlan.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StartPlan : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+//    private lateinit var binding: ActivityMainBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start_plan, container, false)
+    }
+
+    override fun onViewCreated(view:View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view,savedInstanceState)
+        val mimageView: ImageView = view.findViewById(R.id.speakerImg)
+        val mbitmap = (resources.getDrawable(R.drawable.baseline_navigate_next_24) as BitmapDrawable).bitmap
+        val imageRounded = Bitmap.createBitmap(mbitmap.width, mbitmap.height, mbitmap.config)
+        val canvas = Canvas(imageRounded)
+        val mpaint = Paint().apply {
+            isAntiAlias = true
+            shader = BitmapShader(mbitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+        }
+        canvas.drawRoundRect(RectF(20f, 20f, mbitmap.width.toFloat(), mbitmap.height.toFloat()), 300f, 300f, mpaint)
+        mimageView.setImageBitmap(imageRounded)
+    }
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment StartPlan.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            StartPlan().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
